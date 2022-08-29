575c314663bc2988613f0cef696a224530734911
y2k <itwisterlx@gmail.com>
2022-08-29 15:47:52 +0400

    refactoring

--- src/extension/effects.cljs
+++ src/extension/effects.cljs
@@ -1,5 +1,6 @@
 (ns extension.effects
   (:require [extension.domain :as d]
+            [extension.framework :as f]
             [clojure.edn :as edn]))
 
 (defn init []
@@ -12,17 +13,17 @@
               db {:raw-config (if (some? p) p (str d/def-config))
                   :config (if (nil? p) d/def-config (edn/read-string p))}]
           (reset! d/db db)
-          (d/dispatch [:extension.domain/db-changed nil])))))
+          (f/dispatch [:extension.domain/db-changed nil])))))
     (do
       (reset! d/db {:raw-config "{}"
                     :config {:exclude ["READ"]}})
-      (d/dispatch [:extension.domain/db-changed nil]))))
+      (f/dispatch [:extension.domain/db-changed nil]))))
 
 (defonce setup
   (do
-    (d/reg-event-fx
+    (f/reg-event-fx
      :extension.domain/db-reset-requested
      (fn [db]
        (reset! d/db db)
        (js/browser.storage.local.set (clj->js {:key {:value (:raw-config db)}}))
-       (d/dispatch [:extension.domain/db-changed nil])))))
+       (f/dispatch [:extension.domain/db-changed nil])))))
