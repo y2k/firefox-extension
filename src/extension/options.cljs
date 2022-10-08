de37faddd8aa3bcf43131596c395dd849cd185da
y2k <itwisterlx@gmail.com>
2022-10-08 10:32:58 +0400

    refactoring

--- src/extension/options.cljs
+++ src/extension/options.cljs
@@ -1,10 +1,14 @@
 (ns extension.options
   (:require [extension.domain :as d]
+            [extension.storage :as s]
             [extension.effects :as eff]))
+
+(defn document-loaded []
+  (let [node (.-body js/document)]
+    (doseq [cmd (d/make-options [:local (gensym)] (d/get-db) {:type :node :raw-node node})]
+      (eff/execute-command cmd))))
 
 (defonce setup
   (do
-    (let [node (.getElementById js/document "root")]
-      (doseq [cmd (d/make-options [:local (gensym)] (d/get-db) {:type :node :raw-node node})]
-        (eff/execute-command cmd)))
+    (s/load-prefs #'document-loaded)
     nil))
